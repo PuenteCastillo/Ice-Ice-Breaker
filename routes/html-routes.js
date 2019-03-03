839a794b65ea253b31b6ffa984cf7d855a62c805
R2D2 <jp@Joses-MBP.attlocal.net>
2019-03-03 15:07:01 -0800

    files added

---
+++ routes/html-routes.js
@@ -0,0 +1,28 @@
+// *********************************************************************************
+// html-routes.js - this file offers a set of routes for sending users to the various html pages
+// *********************************************************************************
+
+// Dependencies
+// =============================================================
+var path = require("path");
+
+// Routes
+// =============================================================
+module.exports = function(app) {
+
+  // Each of the below routes just handles the HTML page that the user gets sent to.
+
+  // index route loads view.html
+  app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/login.html"));
+  });
+  app.get("/chat", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/chat.html"));
+  });
+
+  app.get("/socializing", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/socializing.html"));
+  });
+
+
+};
